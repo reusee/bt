74b5dbdd01a259bfd7b1b23d5b62eb1fcd096115
reus <reusee@gmail.com>
2019-08-15 23:18:33 +0800

    add trackers

--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module github.com/reusee/bt
 go 1.12
 
 require (
-	github.com/anacrolix/missinggo v1.1.1 // indirect
-	github.com/anacrolix/torrent v1.5.2
+	github.com/RoaringBitmap/roaring v0.4.19 // indirect
+	github.com/anacrolix/dht v1.0.1 // indirect
+	github.com/anacrolix/dht/v2 v2.0.3 // indirect
+	github.com/anacrolix/missinggo v1.2.1 // indirect
+	github.com/anacrolix/torrent v1.6.0
 	github.com/glycerine/go-unsnap-stream v0.0.0-20190730064659-98d31706395a // indirect
 	github.com/mattn/go-sqlite3 v1.11.0 // indirect
 	github.com/reusee/e/v2 v2.3.3
-	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
-	golang.org/x/sys v0.0.0-20190801041406-cbf593c0f2f3 // indirect
+	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
+	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
 )
--- main.go
+++ main.go
@@ -83,6 +83,7 @@ func main() {
 			go func() {
 				t, err := client.AddTorrentFromFile(torrentFile)
 				ce(err)
+				t.AddTrackers(trackers)
 				<-t.GotInfo()
 				t.DownloadAll()
 				for range time.NewTicker(time.Second * 10).C {
@@ -138,6 +139,7 @@ func main() {
 				pt("%s\n", link)
 				t, err := client.AddMagnet(link)
 				ce(err)
+				t.AddTrackers(trackers)
 				<-t.GotInfo()
 				metaInfo := t.Metainfo()
 				t.Drop()
@@ -186,3 +188,185 @@ func main() {
 	case <-c:
 	}
 }
+
+var trackers = func() (ret [][]string) {
+	// from https://github.com/ngosang/trackerslist
+	const text = `
+udp://tracker.coppersurfer.tk:6969/announce
+
+udp://tracker.open-internet.nl:6969/announce
+
+udp://tracker.leechers-paradise.org:6969/announce
+
+udp://tracker.opentrackr.org:1337/announce
+
+udp://tracker.internetwarriors.net:1337/announce
+
+http://tracker.opentrackr.org:1337/announce
+
+http://tracker.internetwarriors.net:1337/announce
+
+udp://9.rarbg.to:2710/announce
+
+udp://9.rarbg.me:2710/announce
+
+udp://tracker.openbittorrent.com:80/announce
+
+http://tracker3.itzmx.com:6961/announce
+
+http://tracker1.itzmx.com:8080/announce
+
+udp://exodus.desync.com:6969/announce
+
+udp://tracker.torrent.eu.org:451/announce
+
+udp://tracker.tiny-vps.com:6969/announce
+
+udp://retracker.lanta-net.ru:2710/announce
+
+udp://tracker2.itzmx.com:6961/announce
+
+udp://tracker.cyberia.is:6969/announce
+
+udp://open.stealth.si:80/announce
+
+udp://open.demonii.si:1337/announce
+
+udp://denis.stalker.upeer.me:6969/announce
+
+http://tracker2.itzmx.com:6961/announce
+
+udp://explodie.org:6969/announce
+
+udp://bt.xxx-tracker.com:2710/announce
+
+http://open.acgnxtracker.com:80/announce
+
+http://explodie.org:6969/announce
+
+udp://tracker4.itzmx.com:2710/announce
+
+http://retracker.mgts.by:80/announce
+
+udp://tracker.moeking.me:6969/announce
+
+udp://torrentclub.tech:6969/announce
+
+udp://ipv4.tracker.harry.lu:80/announce
+
+http://torrentclub.tech:6969/announce
+
+udp://tracker.uw0.xyz:6969/announce
+
+udp://tracker.iamhansen.xyz:2000/announce
+
+udp://zephir.monocul.us:6969/announce
+
+udp://tracker.tvunderground.org.ru:3218/announce
+
+udp://tracker.trackton.ga:7070/announce
+
+udp://tracker.supertracker.net:1337/announce
+
+udp://tracker.nyaa.uk:6969/announce
+
+udp://tracker.lelux.fi:6969/announce
+
+udp://tracker.kamigami.org:2710/announce
+
+udp://tracker.filepit.to:6969/announce
+
+udp://tracker.filemail.com:6969/announce
+
+udp://tracker.dler.org:6969/announce
+
+udp://tracker-udp.gbitt.info:80/announce
+
+udp://retracker.sevstar.net:2710/announce
+
+udp://retracker.netbynet.ru:2710/announce
+
+udp://retracker.maxnet.ua:80/announce
+
+udp://retracker.baikal-telecom.net:2710/announce
+
+udp://retracker.akado-ural.ru:80/announce
+
+udp://newtoncity.org:6969/announce
+
+udp://chihaya.toss.li:9696/announce
+
+udp://bt.dy20188.com:80/announce
+
+https://tracker.vectahosting.eu:2053/announce
+
+https://tracker.lelux.fi:443/announce
+
+https://tracker.gbitt.info:443/announce
+
+https://tracker.fastdownload.xyz:443/announce
+
+https://t.quic.ws:443/announce
+
+https://opentracker.co:443/announce
+
+http://vps02.net.orel.ru:80/announce
+
+http://tracker01.loveapp.com:6789/announce
+
+http://tracker.tvunderground.org.ru:3218/announce
+
+http://tracker.torrentyorg.pl:80/announce
+
+http://tracker.lelux.fi:80/announce
+
+http://tracker.gbitt.info:80/announce
+
+http://tracker.bz:80/announce
+
+http://torrent.nwps.ws:80/announce
+
+http://t.nyaatracker.com:80/announce
+
+http://retracker.sevstar.net:2710/announce
+
+http://open.trackerlist.xyz:80/announce
+
+http://open.acgtracker.com:1096/announce
+
+http://newtoncity.org:6969/announce
+
+http://gwp2-v19.rinet.ru:80/announce
+
+udp://tracker.msm8916.com:6969/announce
+
+https://tracker.publictorrent.net:443/announce
+
+https://1337.abcvg.info:443/announce
+
+http://tracker4.itzmx.com:2710/announce
+
+http://tracker.publictorrent.net:80/announce
+
+http://tracker.bt4g.com:2095/announce
+
+http://t.acg.rip:6699/announce
+
+http://sub4all.org:2710/announce
+
+http://share.camoe.cn:8080/announce
+
+http://bt-tracker.gamexp.ru:2710/announce
+
+http://agusiq-torrents.pl:6969/announce
+
+  `
+	for _, line := range strings.Split(text, "\n") {
+		line = strings.TrimSpace(line)
+		if len(line) == 0 {
+			continue
+		}
+		ret = append(ret, []string{line})
+	}
+	return
+}()
